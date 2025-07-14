0d8ac0fdbee30976a9d6015b1d3a8fd73f33d930
Abhinav Dwivedi <abhinavdwivedi933@gmail.com>
2025-07-14 16:58:27 +0530

    A Mega Blogging App

---
+++ src/Conf/conf.js
@@ -0,0 +1,8 @@
+const conf ={
+  appwrriteurl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_Collection_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_Database_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_Bucket_ID),
+
+}
